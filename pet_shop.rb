7562acea68c00f7afb53b6c6c62ef165d8ccdc66
augustas3000 <augusto.pasaulis@gmail.com>
2020-01-10 14:13:07 +0000

    Completed up to OPTIONALS

--- pet_shop.rb
+++ pet_shop.rb
@@ -15,3 +15,82 @@ def add_or_remove_cash(shop_hash, cash)
   shop_hash[:admin][:total_cash] += cash
 
 end
+
+def pets_sold(shop_hash)
+  return shop_hash[:admin][:pets_sold]
+end
+
+
+def increase_pets_sold(shop_hash, num_pets)
+  shop_hash[:admin][:pets_sold] += num_pets
+end
+
+def stock_count(shop_hash)
+  return shop_hash[:pets].length
+end
+
+# returns an array of pets whose breed name is equal to
+# breed argument given.
+def pets_by_breed(shop_hash, breed)
+  pets_found = []
+  for pet_hash in shop_hash[:pets]
+    if pet_hash[:breed] == breed
+      pets_found.push(breed)
+    end
+  end
+  return pets_found
+end
+
+# returns a pet(hash) based on pet_name argument.
+def find_pet_by_name(shop_hash, pet_name)
+  for pet_hash in shop_hash[:pets]
+    if pet_hash[:name] == pet_name
+      return pet_hash
+    end
+  end
+  # if no matches, we expect a program to return nil.
+  return nil
+end
+
+
+def remove_pet_by_name(shop_hash, pet_name)
+  for pet_hash in shop_hash[:pets]
+    if pet_hash[:name] == pet_name
+      shop_hash[:pets].delete(pet_hash)
+    end
+  end
+end
+
+# add a new pet hash(defined in def setup) into pets array
+# of shop_hash
+def add_pet_to_stock(shop_hash, new_pet_hash)
+  shop_hash[:pets].push(new_pet_hash)
+end
+
+
+# returning cash for a given customer(hash)
+def customer_cash(customer_hash)
+  return customer_hash[:cash]
+end
+
+# subtracts provided su of money from provided customer hash,
+# no need to return anything.
+def remove_customer_cash(customer_hash, money_to_remove)
+  customer_hash[:cash] -= money_to_remove
+end
+
+# return pet count for pet array of inputed customer hash
+def customer_pet_count(customer_hash)
+  return customer_hash[:pets].length
+end
+
+def add_pet_to_customer(customer_hash, new_pet_hash)
+  customer_hash[:pets].push(new_pet_hash)
+end
+
+
+
+
+
+
+#
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -106,75 +106,94 @@ class TestPetShop < Minitest::Test
     assert_equal(990, cash)
   end
 
-  # def test_pets_sold
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(0, sold)
-  # end
+  def test_pets_sold
+    sold = pets_sold(@pet_shop)
+    assert_equal(0, sold)
+  end
 
-  # def test_increase_pets_sold
-  #   increase_pets_sold(@pet_shop,2)
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(2, sold)
-  # end
+  def test_increase_pets_sold
+    increase_pets_sold(@pet_shop,2)
+    # using earlier defined function pets_sold
+    # to return actual value after we run
+    #increase_pets_sold, which does not return
+    # anything
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
 
-  # def test_all_pets_by_breed__found
-  #   pets = pets_by_breed(@pet_shop, "British Shorthair")
-  #   assert_equal(2, pets.count)
-  # end
+    sold = pets_sold(@pet_shop)
+    assert_equal(2, sold)
+  end
 
-  # def test_all_pets_by_breed__not_found
-  #   pets = pets_by_breed(@pet_shop, "Dalmation")
-  #   assert_equal(0, pets.count)
-  # end
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
-  # def test_find_pet_by_name__returns_pet
-  #   pet = find_pet_by_name(@pet_shop, "Arthur")
-  #   assert_equal("Arthur", pet[:name])
-  # end
+  def test_all_pets_by_breed__found
+    pets = pets_by_breed(@pet_shop, "British Shorthair")
+    # pets_by_breed returns an array assigned to pets var.
+    # we the call .count to count how many breeds are in that
+    # array, this is essentialy an actual result (pets.count)
+    # to be compared against
+    assert_equal(2, pets.count)
+  end
 
-  # def test_find_pet_by_name__returns_nil
-  #   pet = find_pet_by_name(@pet_shop, "Fred")
-  #   assert_nil(pet)
-  # end
+  def test_all_pets_by_breed__not_found
+    pets = pets_by_breed(@pet_shop, "Dalmation")
+    assert_equal(0, pets.count)
+  end
 
-  # def test_remove_pet_by_name
-  #   remove_pet_by_name(@pet_shop, "Arthur")
-  #   pet = find_pet_by_name(@pet_shop,"Arthur")
-  #   assert_nil(pet)
-  # end
+  def test_find_pet_by_name__returns_pet
+    pet = find_pet_by_name(@pet_shop, "Arthur")
+    assert_equal("Arthur", pet[:name])
+  end
 
-  # def test_add_pet_to_stock
-  #   add_pet_to_stock(@pet_shop, @new_pet)
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(7, count)
-  # end
+  def test_find_pet_by_name__returns_nil
+    pet = find_pet_by_name(@pet_shop, "Fred")
+    # new assertion method, test passed if test returns nil
+    assert_nil(pet)
+  end
 
-  # def test_customer_cash
-  #   cash = customer_cash(@customers[0])
-  #   assert_equal(1000, cash)
-  # end
+  def test_remove_pet_by_name
+    remove_pet_by_name(@pet_shop, "Tristan")
+    # use the earlier function to return pet hash or nil
+    # assert_nil checks that ths function returns nil for
+    # specific pet we removed with remove_pet_by_name
+    pet = find_pet_by_name(@pet_shop,"Tristan")
+    assert_nil(pet)
+  end
 
-  # def test_remove_customer_cash
-  #   customer = @customers[0]
-  #   remove_customer_cash(customer, 100)
-  #   assert_equal(900, customer[:cash])
-  # end
+  def test_add_pet_to_stock
+    add_pet_to_stock(@pet_shop, @new_pet)
+    count = stock_count(@pet_shop)
+    assert_equal(7, count)
+  end
 
-  # def test_customer_pet_count
-  #   count = customer_pet_count(@customers[0])
-  #   assert_equal(0, count)
-  # end
+  def test_customer_cash
+    cash = customer_cash(@customers[0])
+    assert_equal(1000, cash)
+  end
 
-  # def test_add_pet_to_customer
-  #   customer = @customers[0]
-  #   add_pet_to_customer(customer, @new_pet)
-  #   assert_equal(1, customer_pet_count(customer))
-  # end
+  def test_remove_customer_cash
+    # assign a chosen customer hash from customers array t0  customer variable for testing
+    customer = @customers[0]
+    remove_customer_cash(customer, 100)
+    assert_equal(900, customer[:cash])
+  end
+
+  def test_customer_pet_count
+    # this time customer variable not used but
+    # expression @customers[0] is fed directly to
+    # the count function - no diff..
+    count = customer_pet_count(@customers[0])
+    assert_equal(0, count)
+  end
+
+  def test_add_pet_to_customer
+    customer = @customers[0]
+    # new pet var is pointed to hash as part of setup method
+    add_pet_to_customer(customer, @new_pet)
+    assert_equal(1, customer_pet_count(customer))
+  end
 
   # --- OPTIONAL ---
 
